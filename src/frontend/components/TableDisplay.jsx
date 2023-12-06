3404e18227adcdfb8a3d175e45ea28a22aa6d82d
Kyle Smith <kyle.j.smith1996@gmail.com>
2023-12-06 15:11:34 -0600

    wip

--- src/frontend/components/TableDisplay.jsx
+++ src/frontend/components/TableDisplay.jsx
@@ -7,123 +7,184 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
   Checkbox,
-  Button,
+  IconButton,
   Typography,
 } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const TableDisplay = ({ data }) => {
+const TableDisplay = () => {
+  const [data, setData] = useState([]);
   const [selectedRows, setSelectedRows] = useState([]);
+  const [expandedRows, setExpandedRows] = useState([]);
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = data.map((row) => row.id);
-      setSelectedRows(newSelecteds);
-    } else {
-      setSelectedRows([]);
+  useEffect(() => {
+    fetchData();
+  }, []); // Run once when the component mounts
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:5002/api/getUserData");
+      const userData = await response.json();
+
+      console.log("userData:", userData); // Log the data to inspect its structure
+
+      setData(userData);
+    } catch (error) {
+      console.error("Failed to fetch user data", error);
     }
   };
 
-  const handleClick = (event, id) => {
-    const selectedIndex = selectedRows.indexOf(id);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selectedRows, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selectedRows.slice(1));
-    } else if (selectedIndex === selectedRows.length - 1) {
-      newSelected = newSelected.concat(selectedRows.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selectedRows.slice(0, selectedIndex),
-        selectedRows.slice(selectedIndex + 1)
+  const handleCheckboxChange = (userId) => {
+    const isSelected = selectedRows.includes(userId);
+    if (isSelected) {
+      setSelectedRows((prevSelected) =>
+        prevSelected.filter((id) => id !== userId)
       );
+    } else {
+      setSelectedRows((prevSelected) => [...prevSelected, userId]);
     }
-
-    setSelectedRows(newSelected);
   };
 
-  const isSelected = (id) => selectedRows.indexOf(id) !== -1;
+  const handleSelectAll = (event) => {
+    event.stopPropagation();
+
+    if (selectedRows.length === data.length) {
+      // If all rows are selected, deselect all
+      setSelectedRows([]);
+    } else {
+      // If not all rows are selected, select all
+      const allUserIds = data.map((user) => user.id);
+      setSelectedRows(allUserIds);
+    }
+  };
+
+  const handleRowClick = (userId, event) => {
+    // Check if the click target is not the checkbox
+    if (event.target.tagName !== "INPUT" && event.target.type !== "checkbox") {
+      if (expandedRows.includes(userId)) {
+        setExpandedRows((prevExpanded) =>
+          prevExpanded.filter((id) => id !== userId)
+        );
+      } else {
+        setExpandedRows((prevExpanded) => [...prevExpanded, userId]);
+      }
+    }
+  };
+
+  const handleDeleteSelected = () => {
+    console.log("Deleting selected rows:", selectedRows);
+    // Implement logic to delete selected rows
+  };
 
   return (
     <div>
+      <h2>User Data</h2>
+      {data.length > 0 ? (
+        <TableContainer sx={{ overflowX: "auto" }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Checkbox
+                    checked={
+                      selectedRows.length === data.length && data.length > 0
+                    }
+                    indeterminate={
+                      selectedRows.length > 0 &&
+                      selectedRows.length < data.length
+                    }
+                    onChange={(event) => handleSelectAll(event)}
+                  />
+                </TableCell>
+                <TableCell>ID</TableCell>
+                <TableCell>First Name</TableCell>
+                <TableCell>Last Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Gender</TableCell>
+                <TableCell>Age</TableCell>
+                <TableCell>Country</TableCell>
+                <TableCell>Notification</TableCell>
+                <TableCell>Date of Birth</TableCell>
+                <TableCell>Bio</TableCell>
+                <TableCell>Action</TableCell>
+                {/* Add more columns as needed */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((user) => (
+                <React.Fragment key={user.id}>
+                  <TableRow
+                    onClick={(event) => handleRowClick(user.id, event)}
+                    className={`accordion-row ${
+                      expandedRows.includes(user.id) ? "expanded" : ""
+                    }`}
+                  >
+                    <TableCell>
+                      <Checkbox
+                        checked={selectedRows.includes(user.id)}
+                        onChange={() => handleCheckboxChange(user.id)}
+                      />
+                    </TableCell>
+                    <TableCell>{user.id}</TableCell>
+                    <TableCell>{user.first_name}</TableCell>
+                    <TableCell>{user.last_name}</TableCell>
+                    <TableCell>{user.email}</TableCell>
+                    <TableCell>{user.gender}</TableCell>
+                    <TableCell>{user.age}</TableCell>
+                    <TableCell>{user.country}</TableCell>
+                    <TableCell>{user.notification}</TableCell>
+                    <TableCell>{user.dob}</TableCell>
+                    <TableCell>{user.bio}</TableCell>
+                    <TableCell>
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => console.log("Delete action", user.id)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </TableCell>
+                    {/* Add more cells for additional columns */}
+                  </TableRow>
+                  {expandedRows.includes(user.id) && (
+                    <TableRow className="details-row">
+                      <TableCell colSpan={12}>
+                        {/* Placeholder details content */}
+                        <Typography>
+                          This is a placeholder for additional details.
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </React.Fragment>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <p>No user data available.</p>
+      )}
       {selectedRows.length > 0 && (
-        <Button
-          variant="contained"
-          color="secondary"
-          style={{ margin: "10px" }}
-          onClick={() => {
-            // Implement your delete logic here using selectedRows
-            console.log("Deleting selected rows:", selectedRows);
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "10px",
           }}
         >
-          Delete
-        </Button>
+          <Typography variant="body2" color="textSecondary">
+            {`${selectedRows.length} row${
+              selectedRows.length !== 1 ? "s" : ""
+            } selected`}
+          </Typography>
+          <IconButton
+            aria-label="delete-selected"
+            onClick={handleDeleteSelected}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </div>
       )}
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Checkbox
-                  indeterminate={
-                    selectedRows.length > 0 && selectedRows.length < data.length
-                  }
-                  checked={selectedRows.length === data.length}
-                  onChange={handleSelectAllClick}
-                />
-              </TableCell>
-              <TableCell>ID</TableCell>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Gender</TableCell>
-              <TableCell>Age</TableCell>
-              <TableCell>Country</TableCell>
-              <TableCell>Notification</TableCell>
-              <TableCell>Date of Birth</TableCell>
-              <TableCell>Bio</TableCell>
-              {/* Add more columns as needed */}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row) => (
-              <Accordion key={row.id}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      checked={isSelected(row.id)}
-                      onChange={(event) => {
-                        event.stopPropagation();
-                        handleClick(event, row.id);
-                      }}
-                    />
-                  </TableCell>
-                  <TableCell>{row.id}</TableCell>
-                  <TableCell>{row.first_name}</TableCell>
-                  <TableCell>{row.last_name}</TableCell>
-                  <TableCell>{row.email}</TableCell>
-                  <TableCell>{row.gender}</TableCell>
-                  <TableCell>{row.age}</TableCell>
-                  <TableCell>{row.country}</TableCell>
-                  <TableCell>{row.notification}</TableCell>
-                  <TableCell>{row.dob}</TableCell>
-                  <TableCell>{row.bio}</TableCell>
-                  {/* Add more cells for additional columns */}
-                </AccordionSummary>
-                <AccordionDetails>
-                  {/* Additional information can be added here */}
-                </AccordionDetails>
-              </Accordion>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
     </div>
   );
 };
